c4dab47848256883ee34197d5e962e7a801aaa0c
shrutiapat <112643478+shrutiapat@users.noreply.github.com>
2024-05-31 22:44:06 +0530

    Time: 1 ms (100.00%), Space: 45.6 MB (36.75%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.java
+++ 0260-single-number-iii/0260-single-number-iii.java
@@ -1,14 +1,17 @@
 class Solution {
-            public int[] singleNumber(int[] nums) {
-                HashSet<Integer> h = new HashSet<>();
-                for(int n : nums){
-                    if(h.contains(n)) h.remove(n);
-                    else h.add(n);
-                }
-                Object[] t =h.toArray();
-                int[] result = new int[2];
-                result[0] = (int)t[0];
-                result[1] = (int)t[1];
-                return result;
-            }
+    public int[] singleNumber(int[] nums) {
+        int ans =0 ;
+        for(int i= 0; i < nums.length;i++)
+        {
+            ans = ans ^ nums[i];
+        }
+        ans &= -ans;
+        int[] result = new int[2];
+        for(int n: nums)
+        {
+            if((ans & n) == 0)result[0] ^= n;
+            else result[1] ^= n;
+        }
+        return result;
+    }
 }
