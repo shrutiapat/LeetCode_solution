290c74c3ea95670a61294214b27e1d9d39e6e9a8
shrutiapat <112643478+shrutiapat@users.noreply.github.com>
2024-07-28 23:30:06 +0530

    Time: 40 ms (97.33%), Space: 55.6 MB (94.67%) - LeetHub

--- 2045-second-minimum-time-to-reach-destination/2045-second-minimum-time-to-reach-destination.java
+++ 2045-second-minimum-time-to-reach-destination/2045-second-minimum-time-to-reach-destination.java
@@ -1,24 +1,5 @@
 class Solution {
     public int secondMinimum(int n, int[][] edges, int time, int change) {
-        /**
-            1. traffic light is global
-                - if a departs before b, a will always arrive first
-                    - if a delays bc of red light, b will be delayed as well 
-                - carry the time forward
-                - round=curTime/change
-                    - if round is even, then it is green
-                        - nextTime=curTime+time
-                    - if round is odd, then it is red
-                        - wait
-                        - nextTime=curTime+2*time, (round+1)*change+time
-            2. the question is asking for second minimum time
-                - if A->...->B1->...->C, a second minumum time would be
-                    - A->...->B1->...->B2->...-->C
-                    - a node can be visited twice
-            3. use an timeArr to keep track of the 2nd minimum time for every node
-                - bfs into next only timeArr[i]<nextTime
-         */
-
         List<Integer>[] graph=new ArrayList[n+1];
         for(int i=0;i<n+1;i++) graph[i]=new ArrayList<>();
         for(int[] edge:edges){
