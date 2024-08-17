02917bd2e43688ff6abccf0f4e9dd35125eaae75
shrutiapat <112643478+shrutiapat@users.noreply.github.com>
2024-08-17 23:09:07 +0530

    Time: 49 ms (21.91%), Space: 45 MB (5.96%) - LeetHub

---
+++ 0264-ugly-number-ii/0264-ugly-number-ii.java
@@ -0,0 +1,26 @@
+class Solution {
+
+    public int nthUglyNumber(int n) {
+        PriorityQueue<Long> minHeap = new PriorityQueue<>();
+        Set<Long> seenNumbers = new HashSet<>(); // Set to avoid duplicates
+        int[] primeFactors = { 2, 3, 5 }; // Factors for generating new ugly numbers
+
+        minHeap.offer(1L);
+        seenNumbers.add(1L);
+
+        long currentUgly = 1L;
+        for (int i = 0; i < n; i++) {
+            currentUgly = minHeap.poll(); // Get the smallest number
+
+            // Generate and push the next ugly numbers
+            for (int prime : primeFactors) {
+                long nextUgly = currentUgly * prime;
+                if (seenNumbers.add(nextUgly)) { // Avoid duplicates
+                    minHeap.offer(nextUgly);
+                }
+            }
+        }
+
+        return (int) currentUgly; // Return the nth ugly number
+    }
+}
