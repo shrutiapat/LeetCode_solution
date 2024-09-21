699fff6b9044f72f04308c16c48cb66eaad28206
shrutiapat <112643478+shrutiapat@users.noreply.github.com>
2024-09-21 12:02:38 +0530

    Time: 2 ms (99.27%), Space: 43.3 MB (26.04%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.java
+++ 0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -1,22 +1,23 @@
 class Solution {
-    public List<List<Integer>> combinationSum2(int[] cand, int target) {
-    Arrays.sort(cand);
-    List<List<Integer>> res = new ArrayList<List<Integer>>();
-    List<Integer> path = new ArrayList<Integer>();
-    dfs_com(cand, 0, target, path, res);
-    return res;
-}
-void dfs_com(int[] cand, int cur, int target, List<Integer> path, List<List<Integer>> res) {
-    if (target == 0) {
-        res.add(new ArrayList(path));
-        return ;
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        List < List < Integer >> ans = new ArrayList < > ();
+        Arrays.sort(candidates);
+        findCombinations(0, candidates, target, ans, new ArrayList < > ());
+        return ans;
     }
-    if (target < 0) return;
-    for (int i = cur; i < cand.length; i++){
-        if (i > cur && cand[i] == cand[i-1]) continue;
-        path.add(path.size(), cand[i]);
-        dfs_com(cand, i+1, target - cand[i], path, res);
-        path.remove(path.size()-1);
+    static void findCombinations(int ind, int[] arr, int target, List < List < Integer >> ans, List < Integer > ds) {
+        if (target == 0) {
+            ans.add(new ArrayList < > (ds));
+            return;
+        }
+
+        for (int i = ind; i < arr.length; i++) {
+            if (i > ind && arr[i] == arr[i - 1]) continue;
+            if (arr[i] > target) break;
+
+            ds.add(arr[i]);
+            findCombinations(i + 1, arr, target - arr[i], ans, ds);
+            ds.remove(ds.size() - 1);
+        }
     }
-}
 }
