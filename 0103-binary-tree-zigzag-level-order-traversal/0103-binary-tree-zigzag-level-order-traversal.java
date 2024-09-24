49d3f7f96ca64829783d38c103eda499511a6d92
shrutiapat <112643478+shrutiapat@users.noreply.github.com>
2024-09-24 15:05:12 +0530

    Time: 0 ms (100.00%), Space: 42.5 MB (8.71%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
@@ -15,62 +15,28 @@
  */
 class Solution {
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
-
-        // If the root is null, return an empty list since there's no tree to traverse.
-        if (root == null)
-            return new ArrayList<>();
-
-        // This will hold the final zigzag level order traversal.
-        List<List<Integer>> res = new ArrayList<>();
-
-        // Queue to help in level order traversal.
+        List<List<Integer>> ans = new ArrayList<>();
+        if (root == null) return ans;
         Queue<TreeNode> q = new LinkedList<>();
-
-        // Start the traversal by adding the root to the queue.
-        q.offer(root);
-
-        // A boolean to track the direction of traversal.
-        // Start from left to right.
-        boolean leftToRight = true;
-
-        while (!q.isEmpty()) {
-
-            // Get the number of nodes at the current level.
+        q.offer(root); boolean flag = false;
+        while(!q.isEmpty()){
             int size = q.size();
-
-            // This list will hold the nodes' values for the current level.
-            List<Integer> curLevel = new ArrayList<>(size);
-
-            for (int i = 0; i < size; i++) {
-
-                // Poll the first node in the queue.
+            LinkedList<Integer> l = new LinkedList<>();
+            while(size-- > 0){
                 TreeNode node = q.poll();
-
-                // Add the node's value to the current level's list.
-                curLevel.add(node.val);
-
-                // If the node has a left child, add it to the queue.
-                if (node.left != null)
-                    q.offer(node.left);
-
-                // If the node has a right child, add it to the queue.
-                if (node.right != null)
-                    q.offer(node.right);
+                if(flag){
+                    l.addFirst(node.val);
+                }
+                else{
+                    l.add(node.val);
+                }
+                if(node.left != null){q.offer(node.left);}
+                if(node.right != null){q.offer(node.right);}
             }
-
-            // If the current level needs to be right-to-left, reverse the list.
-            if (!leftToRight) {
-                Collections.reverse(curLevel);
-            }
-
-            // Add the current level's list to the result list.
-            res.add(curLevel);
-
-            // Toggle the direction for the next level.
-            leftToRight = !leftToRight;
+            flag = !flag;
+            ans.add(l);
         }
-
-        // Return the final zigzag level order traversal.
-        return res;
+        return ans;
+        
     }
 }
