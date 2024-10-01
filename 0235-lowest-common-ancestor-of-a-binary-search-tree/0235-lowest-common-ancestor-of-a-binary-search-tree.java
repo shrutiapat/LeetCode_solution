7c5cff57abf1312988ca9b75cd7803fa32701f1b
shrutiapat <112643478+shrutiapat@users.noreply.github.com>
2024-10-01 19:43:37 +0530

    Time: 5 ms (100.00%), Space: 44.8 MB (67.33%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.java
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.java
@@ -10,15 +10,15 @@
 
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        int small = Math.min(p.val, q.val);
-        int large = Math.max(p.val, q.val);
-        while (root != null) {
-            if (root.val > large) // p, q belong to the left subtree
-                root = root.left;
-            else if (root.val < small) // p, q belong to the right subtree
-                root = root.right;
-            else // Now, small <= root.val <= large -> This root is the LCA between p and q
-                return root;
+        TreeNode curr = root;
+        while(curr != null){
+            if(p.val<curr.val && q.val <curr.val){
+                curr = curr.left;
+            }
+            else if(p.val > curr.val && q.val > curr.val){
+                curr = curr.right;
+            }
+            else return curr;
         }
         return null;
     }
