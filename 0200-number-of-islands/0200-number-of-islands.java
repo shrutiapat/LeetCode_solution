e804760a42abb34345ee12c83080135a197f5895
shrutiapat <112643478+shrutiapat@users.noreply.github.com>
2025-03-08 06:14:41 +0530

    Time: 3 ms (86.57%), Space: 49.9 MB (34.79%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,57 +1,26 @@
 class Solution {
-    boolean[][] flag;
+    public void solve(int r, int c, char[][] grid){
+        int n = grid.length, m = grid[0].length;
+        if(r<0 || r>=n || c<0 || c>=m || grid[r][c] == '0') return;
+
+        grid[r][c] = '0';
+        solve(r+1,c,grid);
+        solve(r-1,c,grid);
+        solve(r,c+1,grid);
+        solve(r,c-1,grid);
+
+    }
     public int numIslands(char[][] grid) {
-        int height = grid.length;
-        int width = grid[0].length;
-        int n=0;
-        flag = new boolean[height][width];
-        for(int i=0; i<height; i++){
-            for(int j=0; j<width;j++){
-                if(grid[i][j] == '1' && !flag[i][j]){
-                    flag[i][j] = true;
-                    ++n;
-                    inrowchecker(grid,i,j);
-                    incolumnchecker(grid,i,j);
+        // Code here
+        int n = grid.length, m = grid[0].length, ans = 0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j] == '1'){
+                    ans++;
+                    solve(i,j,grid);
                 }
             }
         }
-        return n;
-    }
-    private void inrowchecker(char[][] grid, int row, int cols){
-        for(int i=cols-1; i>-1; i--){
-            if(grid[row][i] == '1' && !flag[row][i]){
-                flag[row][i] = true;
-                incolumnchecker(grid,row,i);
-            }
-            else break;
-        }
-
-        for(int i=cols+1; i<grid[0].length; i++){
-            if(grid[row][i] == '1' && !flag[row][i]){
-                flag[row][i] = true;
-                incolumnchecker(grid,row,i);
-            }
-            else break;
-        }
-        return;
-    }
-
-      private void incolumnchecker(char[][] grid, int row, int cols){
-        for(int i=row-1; i>-1; i--){
-            if(grid[i][cols] == '1' && !flag[i][cols]){
-                flag[i][cols] = true;
-                inrowchecker(grid,i,cols);
-            }
-            else break;
-        }
-
-        for(int i=row+1; i<grid.length; i++){
-            if(grid[i][cols] == '1' && !flag[i][cols]){
-                flag[i][cols] = true;
-                inrowchecker(grid,i,cols);
-            }
-            else break;
-        }
-        return;
+        return ans;
     }
 }
