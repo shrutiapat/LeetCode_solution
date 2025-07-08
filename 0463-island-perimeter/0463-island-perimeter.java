cd8aacb139a8e72d0c36b8bba4f4ae59255b77b7
shrutiapat <112643478+shrutiapat@users.noreply.github.com>
2025-07-09 02:23:58 +0530

    Time: 4 ms (99.88%), Space: 44.9 MB (99.23%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.java
+++ 0463-island-perimeter/0463-island-perimeter.java
@@ -1,17 +1,45 @@
-class Solution {
-     public int islandPerimeter(int[][] grid) {
-        int islands = 0, neighbours = 0;
+// class Solution {
+//     public int islandPerimeter(int[][] grid) {
+//         int peri = 0;
+//         int row = grid.length;
+//         int col = grid[0].length;
+//         for(int i =0; i<row; i++){
+//             for (int j=0; j<col; j++){
+//                 if(grid[i][j]==1) peri += 4;{
+//                     if(i>0 && grid[i-1][j]==1) peri -= 2;
+//                     if(j>0 && grid[i][j-1]==1) peri -= 2;
+//                 }
+                
+//             }
+//         }
+//         return peri;
 
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[i].length; j++) {
+//     }
+// }
+
+class Solution {
+    public int islandPerimeter(int[][] grid) {
+        int perimeter = 0;
+        
+        int rows = grid.length;
+        int cols = grid[0].length;
+        
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
                 if (grid[i][j] == 1) {
-                    islands++; 
-                    if (i < grid.length - 1 && grid[i + 1][j] == 1) neighbours++; 
-                    if (j < grid[i].length - 1 && grid[i][j + 1] == 1) neighbours++; 
+                    perimeter += 4;
+                    
+                    // Subtract shared edge with upper neighbor
+                    if (i > 0 && grid[i - 1][j] == 1)
+                        perimeter -= 2;
+                    
+                    // Subtract shared edge with left neighbor
+                    if (j > 0 && grid[i][j - 1] == 1)
+                        perimeter -= 2;
                 }
             }
         }
-
-        return islands * 4 - neighbours * 2;
+        
+        return perimeter;
     }
 }
