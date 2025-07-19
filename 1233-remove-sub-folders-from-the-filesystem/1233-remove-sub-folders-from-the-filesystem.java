ffeb682b991eb7486a0cefc9cdaef1dd19b4a80c
shrutiapat <112643478+shrutiapat@users.noreply.github.com>
2025-07-20 00:45:25 +0530

    Time: 40 ms (81.05%), Space: 55.2 MB (84.38%) - LeetHub

--- 1233-remove-sub-folders-from-the-filesystem/1233-remove-sub-folders-from-the-filesystem.java
+++ 1233-remove-sub-folders-from-the-filesystem/1233-remove-sub-folders-from-the-filesystem.java
@@ -1,66 +1,18 @@
 class Solution {
-
-    static class TrieNode {
-
-        boolean isEndOfFolder;
-        HashMap<String, TrieNode> children;
-
-        TrieNode() {
-            this.isEndOfFolder = false;
-            this.children = new HashMap<>();
-        }
-    }
-
-    TrieNode root;
-
-    Solution() {
-        this.root = new TrieNode();
-    }
-
     public List<String> removeSubfolders(String[] folder) {
-        // Build Trie from folder paths
-        for (String path : folder) {
-            TrieNode currentNode = root;
-            String[] folderNames = path.split("/");
-
-            for (String folderName : folderNames) {
-                // Skip empty folder names
-                if (folderName.equals("")) continue;
-                // Create new node if it doesn't exist
-                if (!currentNode.children.containsKey(folderName)) {
-                    currentNode.children.put(folderName, new TrieNode());
-                }
-                currentNode = currentNode.children.get(folderName);
+        ArrayList<String> ans = new ArrayList<>();
+        Arrays.sort(folder);
+        int i=0;
+        ans.add(folder[i++]);
+        String prev=folder[0];
+        while(i < folder.length){
+            String curr=folder[i];
+            if(!curr.startsWith(prev+'/')){
+                prev = folder[i];
+                ans.add(prev);
             }
-            // Mark the end of the folder path
-            currentNode.isEndOfFolder = true;
+            i++;
         }
-
-        // Check each path for subfolders
-        List<String> result = new ArrayList<>();
-        for (String path : folder) {
-            TrieNode currentNode = root;
-            String[] folderNames = path.split("/");
-            boolean isSubfolder = false;
-
-            for (int i = 0; i < folderNames.length; i++) {
-                // Skip empty folder names
-                if (folderNames[i].equals("")) continue;
-
-                TrieNode nextNode = currentNode.children.get(folderNames[i]);
-                // Check if the current folder path is a subfolder of an
-                // existing folder
-                if (nextNode.isEndOfFolder && i != folderNames.length - 1) {
-                    isSubfolder = true;
-                    break; // Found a sub-folder
-                }
-
-                currentNode = nextNode;
-            }
-            // If not a sub-folder, add to the result
-            if (!isSubfolder) result.add(path);
-        }
-
-        return result;
+        return ans;
     }
 }
